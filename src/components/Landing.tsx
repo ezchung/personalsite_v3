3931ebef24608d4836973c0a7d6f438a6a09efa9
Ezra Chung <ezra.chung97@gmail.com>
2024-09-12 21:34:09 -0500

    Initial layout for landing section

---
+++ src/components/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Landing.css';
+
+interface Props {}
+
+function Landing(props: Props): JSX.Element {
+  return (
+    <section className="landing">
+      <h1>Ezra Chung is a software engineer</h1>
+    </section>
+  );
+};
+
+export default Landing;
