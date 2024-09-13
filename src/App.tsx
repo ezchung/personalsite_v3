f7a17eb5093303e15c768a59dbc40d93e9ff0bc4
Ezra Chung <ezra.chung97@gmail.com>
2024-09-13 11:11:14 -0500

    moving original code away

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './components/Landing';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* Start of APPLICATION */}
       <div className="App">
         <Landing />
       </div>
-      <div>
+    </>
+  )
+}
+
+export default App
+
+
+/**
+ *     before return   const [count, setCount] = useState(0)    
+ *     <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> 
+ *    <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -33,8 +40,4 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
-
-export default App
+ */
