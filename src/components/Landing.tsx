db852ff3cefc20e1c2d6b86eb211da34ac045158
Ezra Chung <ezra.chung97@gmail.com>
2024-09-13 16:47:53 -0500

    Remove animated symbols component for now

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Landing.css';
 
 import ResumeLink from './ResumeLink';
+import AnimatedSymbols from './AnimatedSymbols';
 //import AnimatedSymbols from './AnimatedSymbols';
 //can add later. trying to use z-index to bring this text out to the front
 
@@ -10,12 +11,12 @@ interface Props {}
 const Landing: React.FC<Props> = () => {
   return (
     <section className="landing">
-      <div className="background">
-        <h1>Ezra Chung is a software engineer</h1>
-        <button>
-            <ResumeLink />
-        </button>
-      </div>
+        <div className="background">
+            <h1>Ezra Chung is a software engineer</h1>
+            <button>
+                <ResumeLink />
+            </button>
+        </div>
     </section>
   );
 };
