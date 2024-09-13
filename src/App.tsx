683f6d5b06649e0641ad11474dcbb9bbe3e59dea
Ezra Chung <ezra.chung97@gmail.com>
2024-09-12 22:25:07 -0500

    Added Landing component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Landing from './components/Landing';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* Start of APPLICATION */}
+      <div className="App">
+        <Landing />
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
