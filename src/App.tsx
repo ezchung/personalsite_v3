c40a864d7719586efdfa742019ad20430a4cf13a
Ezra Chung <ezra.chung97@gmail.com>
2024-09-15 01:47:51 -0500

    deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import './App.css'
-import Landing from './components/Landing';
-import Router from './components/Router';
+import Intro from './components/Sections/Intro';
+import Landing from './components/Sections/Landing';
+import Router from './components/Sections/Router';
 
 function App() {
   return (
     <div className="App">
       <Router />
-        <section>
-          <Landing />
-        </section>
+      <Landing />
+      <Intro />
+      <Landing></Landing>
         <section>
           Parallax
         </section>
         <section>
-          Introduction
+          <Intro />
         </section>
         <section>
           Parallax
@@ -32,30 +33,3 @@ function App() {
 }
 
 export default App
-/**
- * // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'    
- * 
- * before return   const [count, setCount] = useState(0)    
- *     <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> 
- *    <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
- */
