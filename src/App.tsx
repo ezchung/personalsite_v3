adc99def8499b55e83090bafb1bf483ab63d7077
Ezra Chung <ezra.chung97@gmail.com>
2024-09-15 23:40:40 -0500

    reorganized strucutre because we will be rerouting to a new page at one point so had Router as the main point to what page to show

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import './App.css'
-import Contact from './components/Sections/Contact';
-import Experience from './components/Sections/Experience';
-import Intro from './components/Sections/Intro';
-import Landing from './components/Sections/Landing';
-import ProjectsSection from './components/Sections/ProjectsSection';
-import Router from './components/Sections/Router';
+import Router from './components/Sections/Router'
+
 
 function App() {
   return (
     <div className="App">
       <Router />
-      <main>
-        <section id="home"><Landing /></section>
-        <section id="about"><Intro /></section>
-        <section id="projects"><ProjectsSection /></section>
-        <section id="experience"><Experience /></section>
-        <section id="contact"><Contact /></section>
-      </main>
     </div>
   )
 }
