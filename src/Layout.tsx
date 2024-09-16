e2711fb31cc7850f77a0d257f64100cb91fa81c6
Ezra Chung <ezra.chung97@gmail.com>
2024-09-15 23:41:09 -0500

    This will be the main home page

---
+++ src/Layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import NavBar from './components/Sections/Navbar'
+import Landing from './components/Sections/Landing'
+import Intro from './components/Sections/Intro'
+import Experience from './components/Sections/Experience'
+import Contact from './components/Sections/Contact'
+import App from './App'
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <section id="home"><Landing /></section>
+        <section id="about"><Intro /></section>
+        <section id="experience"><Experience /></section>
+        <section id="contact"><Contact /></section>
+      </main>
+    </>
+  )
+}
+
+export default Layout
