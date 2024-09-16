d2b6a26ec30d847db35745346c4a2ebd39a4a6a3
Ezra Chung <ezra.chung97@gmail.com>
2024-09-15 23:39:46 -0500

    made changes to scroll to the sections instead of adding to the top

--- src/components/Sections/Navbar.tsx
+++ src/components/Sections/Navbar.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import ResumeLink from '../Reusuables/ResumeLink'
+import { Link, useNavigate } from 'react-router-dom'
 import './Navbar.css'
+import ResumeLink from '../Reusuables/ResumeLink'
 
 const NavBar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const scrollToSection = (sectionId: string) => {
+    navigate('/');
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
+  };
+
   return (
-    <div className='navbar'>
+    <nav className='navbar'>
       <div className='wrapper'>
-        <Link to="/" className="logo">EZ</Link>
+        <Link to="/" className="logo" onClick={() => scrollToSection('home')}>EZ</Link>
         <div className='non_logos'>
-          <Link to="/about">About Me</Link>
+          <a href="#about" onClick={(e) => { e.preventDefault(); scrollToSection('about'); }}>About Me</a>
           <Link to="/projects">Projects</Link>
-          <Link to="/experience">Experience</Link>
+          <a href="#experience" onClick={(e) => { e.preventDefault(); scrollToSection('experience'); }}>Experience</a>
+          <a href="#contact" onClick={(e) => { e.preventDefault(); scrollToSection('contact'); }}>Contact</a>
         </div>
         <button className='resume_btn'><ResumeLink /></button>
       </div>
-    </div>
+    </nav>
   )
 }
-
-//going to navbar to link to the sections
 
 export default NavBar
